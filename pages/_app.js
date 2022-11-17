a13622a6d50ff4e54d62cef5a11914a3cb6d747d
potoostudio <ideas@potoo.studio>
2022-11-17 10:18:23 -0700

    fix script id

--- pages/_app.js
+++ pages/_app.js
@@ -12,9 +12,9 @@ function MyApp({ Component, pageProps, router }, AppProps) {
 
   return (
       <>
-          <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+          <Script id='GA-script' strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
-          <Script strategy="lazyOnload">
+          <Script strategy="lazyOnload" id='GA-script-two'>
               {`
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
